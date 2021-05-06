6472b4c18bf08b159330a64916cf4c5476eb8914
Vitor Guedes <vittorbassdev@gmail.com>
2021-05-06 20:12:54 -0300

    Refectory Arquivo de rotas Index

--- sw_frontend/src/index.js
+++ sw_frontend/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/index.css';
-import Home from './pages/Home';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
